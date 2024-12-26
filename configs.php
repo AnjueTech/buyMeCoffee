c581acc56e002234a4c5d5913cbcb9e3b43cb0c6
ALEX NJUE WAMBUI <78609206+AnjueTech@users.noreply.github.com>
2024-12-26 05:08:26 -0800

    Add files via upload

---
+++ configs.php
@@ -0,0 +1,3 @@
+<?php
+$SecretKey = "sk_live_b708aaced9c28e34c45b818a768620fd11e858f6"; // Add your secret key here. Remember to change this to your live secret key in production
+$PublicKey = "pk_live_6afe9e1d099119ae4581bd05c1311bcce84d7929"; // Add your public key here. Remember to change this to your live public key in production
